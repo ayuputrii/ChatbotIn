7b59cd819963736b7d7db247aa47a686efa5c731
Ayu Armadani Putri Mahirun <ayuarmadani.pm@gmail.com>
2021-12-29 02:50:21 +0700

    Update quick replies

--- src/utils/quick-replies.js
+++ src/utils/quick-replies.js
@@ -1,8 +1,8 @@
 const quickReplies = [
   {text: 'Apakah kamu tahu tentang Lenna AI ?', value: 'what'},
   {text: 'Dimanakah alamat kantor Lenna AI ?', value: 'where'},
-  {text: 'Produk apa saja yang telah Lenna AI buat ?', value: 'products'},
   {text: 'Saya tertarik dengan Lenna AI', value: 'interest'},
+  {text: 'Produk apa saja yang telah Lenna AI buat ?', value: 'products'},
 ];
 
 export default quickReplies;
