d39716cc58f967e386fe7bf39ee95008874438bd
Ayu Armadani Putri Mahirun <ayuarmadani.pm@gmail.com>
2021-12-28 16:32:37 +0700

    Add splash screen

--- android/app/src/main/java/com/chatbotin/MainActivity.java
+++ android/app/src/main/java/com/chatbotin/MainActivity.java
@@ -2,8 +2,16 @@ package com.chatbotin;
 
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
+public class MainActivity extends ReactActivity {
+  @Override
+    protected void onCreate(Bundle savedInstanceState){
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
+      
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
