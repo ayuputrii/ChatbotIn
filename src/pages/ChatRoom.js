f5c31fd1f9fc8d25b0b2e60adedfafd18fc7e6f1
Ayu Armadani Putri Mahirun <ayuarmadani.pm@gmail.com>
2021-12-28 17:33:33 +0700

    Update chat room

--- src/pages/ChatRoom.js
+++ src/pages/ChatRoom.js
@@ -7,13 +7,14 @@ import {
   Text,
   ScrollView,
   TouchableOpacity,
+  Image,
 } from 'react-native';
 import {Dialogflow_V2} from 'react-native-dialogflow';
-import {GiftedChat} from 'react-native-gifted-chat';
-import {dialogFlowConfig} from './env';
-import quickReplies from './utils/quick-replies';
+import {Bubble, GiftedChat, InputToolbar, Send} from 'react-native-gifted-chat';
+import {dialogFlowConfig} from '../../env';
+import quickReplies from '../utils/quick-replies';
 
-const avatarBot = require('./assets/images/2.jpg');
+const avatarBot = require('../assets/images/2.jpg');
 
 const activeBot = {
   _id: 2,
@@ -21,7 +22,7 @@ const activeBot = {
   avatar: avatarBot,
 };
 
-const App = () => {
+const ChatRoom = () => {
   const [messages, setMessages] = useState([
     {
       _id: 2,
@@ -95,33 +96,55 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.sectionContainer}>
-      <StatusBar barStyle="light-content" backgroundColor="gray" />
+      <StatusBar barStyle="light-content" backgroundColor="#009dae" />
       <View style={styles.sectionHeader}>
         <Text style={styles.sectionTitle}>ChatbotIn</Text>
       </View>
-      <View style={styles.sectionContainer}>
-        <GiftedChat
-          messages={messages}
-          onSend={message => onSend(message)}
-          user={{_id: 1}}
-          renderChatFooter={() => (
-            <View style={styles.quickReply}>
-              <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-                <View style={styles.viewTouchableOpacity}>
-                  {quickReplies.map((quickReply, i) => (
-                    <TouchableOpacity
-                      onPress={() => onQuickReplyPress(quickReply, i)}
-                      style={styles.quickReplyOption}
-                      key={i}>
-                      <Text>{quickReply.text}</Text>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-              </ScrollView>
-            </View>
-          )}
-        />
-      </View>
+      <GiftedChat
+        alignTop
+        alwaysShowSend={true}
+        messages={messages}
+        onSend={message => onSend(message)}
+        user={{_id: 1}}
+        renderSend={props => {
+          return (
+            <Send {...props}>
+              <View style={styles.iconSend}>
+                <Image source={avatarBot} style={styles.icon} />
+              </View>
+            </Send>
+          );
+        }}
+        renderInputToolbar={props => {
+          return (
+            <InputToolbar
+              {...props}
+              containerStyle={styles.inputContainer}
+              textInputProps={{
+                style: styles.inputToolbar,
+              }}
+            />
+          );
+        }}
+        renderChatFooter={() => (
+          <View style={styles.quickReply}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <View style={styles.viewTouchableOpacity}>
+                {quickReplies.map((quickReply, i) => (
+                  <TouchableOpacity
+                    onPress={() => onQuickReplyPress(quickReply, i)}
+                    style={styles.quickReplyOption}
+                    key={i}>
+                    <Text style={styles.textTouchableOpacity}>
+                      {quickReply.text}
+                    </Text>
+                  </TouchableOpacity>
+                ))}
+              </View>
+            </ScrollView>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
@@ -129,11 +152,13 @@ const App = () => {
 const styles = StyleSheet.create({
   sectionContainer: {
     flex: 1,
+    backgroundColor: '#fff',
+    position: 'relative',
   },
   sectionHeader: {
     height: 60,
     padding: 10,
-    backgroundColor: 'gray',
+    backgroundColor: '#009dae',
     borderBottomRightRadius: 24,
     borderBottomLeftRadius: 24,
     elevation: 6,
@@ -143,10 +168,41 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: '600',
     textAlign: 'center',
+    fontFamily: 'PatrickHand-Regular',
+  },
+  inputContainer: {
+    marginBottom: 6,
+    borderWidth: 0,
+    borderColor: 'transparent',
+    backgroundColor: 'transparent',
+  },
+  inputToolbar: {
+    width: '84%',
+    height: '80%',
+    fontSize: 16,
+    color: 'black',
+    paddingTop: 6,
+    paddingLeft: 14,
+    marginLeft: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#009dae',
+  },
+  iconSend: {
+    marginHorizontal: 2,
+    top: 5,
+    elevation: 5,
+  },
+  icon: {
+    height: 40,
+    width: 40,
   },
   quickReply: {
     paddingHorizontal: 10,
-    marginVertical: 15,
+    marginVertical: 20,
+    position: 'absolute',
+    bottom: 0,
   },
   viewTouchableOpacity: {
     flexDirection: 'row',
@@ -157,8 +213,11 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginRight: 10,
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: '#009dae',
+  },
+  textTouchableOpacity: {
+    fontFamily: 'PatrickHand-Regular',
   },
 });
 
-export default App;
+export default ChatRoom;
