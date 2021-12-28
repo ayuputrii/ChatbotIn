c16a98e2b9e7fc29b6213adccd459e71685b52e2
Ayu Armadani Putri Mahirun <ayuarmadani.pm@gmail.com>
2021-12-28 22:16:59 +0700

    Update quick replies

--- src/utils/quick-replies.js
+++ src/utils/quick-replies.js
@@ -1,6 +1,7 @@
 const quickReplies = [
   {text: 'Apakah kamu tahu tentang Lenna AI ?', value: 'what'},
   {text: 'Dimanakah alamat kantor Lenna AI ?', value: 'where'},
+  {text: 'Produk apa saja yang telah Lenna AI buat ?', value: 'products'},
   {text: 'Saya tertarik dengan Lenna AI', value: 'interest'},
 ];
 
